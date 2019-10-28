6eb059480cd58a6190047b14fcd19547cf765267
ismatovkan <56656162+ismatovkan@users.noreply.github.com>
2019-10-28 17:44:35 -0400

    check update

---
+++ src/main/java/lion.java
@@ -0,0 +1,10 @@
+public class lion {
+    public static void main(String[] args) {
+        String Lionname = "Simba";
+        int age = '4';
+        float myFloatNum = 30f;
+
+
+
+    }
+}
---
+++ src/test/java/EntryPoint.java
@@ -0,0 +1,12 @@
+public class EntryPoint {
+    public static void main(String[] args) {
+        Animal myCat = new Animal();
+        myCat.name = "Gav";
+        myCat.voice = "Meayu!";
+        System.out.println("");
+        Animal raufsCat = new Animal();
+        raufsCat.name = "Leila";
+        raufsCat.voice = "Meayu!";
+        System.out.println("");
+    }
+}
