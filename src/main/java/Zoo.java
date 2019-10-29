f40a97b3b0b890cfce6373f340f07634f21a1ca3
ismatovkan <56656162+ismatovkan@users.noreply.github.com>
2019-10-28 20:56:29 -0400

    check update

---
+++ src/main/java/Zoo.java
@@ -0,0 +1,12 @@
+public class Zoo {
+    public static void main(String[] args) {
+        lion a = new lion();
+        a. setName("Simba");
+        a. setAge(4);
+        a. setWeight(50.3);
+
+        System.out.println(a.getName());
+        System.out.println(a.getAge());
+        System.out.println(a.getWeight());
+    }
+}
